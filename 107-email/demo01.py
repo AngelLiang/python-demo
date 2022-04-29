204ed3c3b8e5134c6aace5291ec83b1297257d93
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2022-04-29 18:50:03 +0800

    feat: 添加python发送邮件示例

---
+++ 107-email/demo01.py
@@ -0,0 +1,34 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# 第三方 SMTP 服务
+mail_host = os.getenv('MAIL_HOST')
+mail_user = os.getenv('MAIL_USER')
+mail_pass = os.getenv('MAIL_PASS')
+
+
+sender = os.getenv('MAIL_SENDER')
+# 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+receivers = os.getenv('MAIL_RECEIVERS')
+if ',' in receivers:
+    receivers = receivers.split(',')
+
+message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
+message['From'] = Header("发送测试", 'utf-8')
+message['To'] = Header("测试", 'utf-8')
+
+subject = 'Python SMTP 邮件测试'
+message['Subject'] = Header(subject, 'utf-8')
+
+
+try:
+    smtpObj = smtplib.SMTP()
+    smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+    smtpObj.login(mail_user, mail_pass)
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException:
+    print("Error: 无法发送邮件")
--- README.md
+++ README.md
@@ -8,5 +8,4 @@
 | 104  | nacos-sdk-python          | python接入nacos               |
 | 105  | aliyun-send-message       | 阿里云旧版SDK发送短信（未成功） |
 | 106  | alibabacloud-send-message | 阿里云新版SDK发送短信         |
-
-
+| 107  | email                     | 使用Python发送邮件            |
